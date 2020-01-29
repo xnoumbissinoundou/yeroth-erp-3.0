d5f01f4a84eecc660a204e0bbdc9636d5aec7534
a user <user@gmail.com>
2020-01-29 17:50:42 +0100

    Sur la branche master 	modifié :         yeroth-erp-3-0-development-scripts/yeroth-WINDOWS-setup-yeroth_windows_branch.sh

--- yeroth-erp-3-0-development-scripts/yeroth-WINDOWS-setup-yeroth_windows_branch.sh
+++ yeroth-erp-3-0-development-scripts/yeroth-WINDOWS-setup-yeroth_windows_branch.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 cp yeroth-erp-3-0-development-scripts/yeroth-WINDOWS-compile_yeroth-erp-3-0.sh yeroth-erp-3-0-development-scripts/yeroth-compile_yeroth-erp-3-0.sh
-git rm *.pdf
 git rm -r yeroth-erp-3-0-development-scripts/yeroth-create-dpkg*
 git rm -r yeroth-erp-3-0-deb-file-repository
 git rm -r yeroth-erp-3.0-academic
 git rm -r yeroth-erp-3.0-standalone
 git rm -r yeroth-erp-3-0-gnome-files-desktop
-git rm -r doc
 git rm -r test
 git rm bin/*.sh
