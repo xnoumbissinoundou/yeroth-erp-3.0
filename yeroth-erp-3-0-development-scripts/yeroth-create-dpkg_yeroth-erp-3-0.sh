091796cec6f82d45732e08532be438c208a56b94
Xavier NOUMBISSI NOUNDOU, Dipl.-Inf., Ph.D. (ABD) <xnoumbissi@gmail.com>
2020-02-29 04:08:46 -0500

    On branch master 	modified:   yeroth-erp-3-0-development-scripts/yeroth-create-dpkg_yeroth-erp-3-0.sh

--- yeroth-erp-3-0-development-scripts/yeroth-create-dpkg_yeroth-erp-3-0.sh
+++ yeroth-erp-3-0-development-scripts/yeroth-create-dpkg_yeroth-erp-3-0.sh
@@ -159,8 +159,8 @@ USER_NAME=\"yeroth_erp_3\"
 PASSWDDB=\"1234567\"
 echo \"Enter MariaDB administrative password YEROTH-ERP-3.0 database and user!\"
 mysql -uroot -p -e \"CREATE DATABASE \${MAINDB} CHARACTER SET = 'utf8'; \
-CREATE USER \${USER_NAME}@localhost IDENTIFIED BY '\${PASSWDDB}'; \
-GRANT ALL PRIVILEGES ON \${MAINDB}.* TO '\${USER_NAME}'@'localhost'; \
+CREATE USER \${USER_NAME}@'%' IDENTIFIED BY '\${PASSWDDB}'; \
+GRANT ALL PRIVILEGES ON *.* TO '\${USER_NAME}'@'%'; \
 FLUSH PRIVILEGES;\"
 mysql_ret_code=\"\$?\"
 exit 0"
