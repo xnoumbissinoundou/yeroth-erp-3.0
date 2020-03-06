514b04d2edbc0bdf5df17541bc60ebdfe5fec9fe
Xavier NOUMBISSI NOUNDOU, Dipl.-Inf., Ph.D. (ABD) <xnoumbissi@gmail.com>
2020-03-06 21:19:37 +0100

    On branch master 	modified:   src/utils/yeroth-erp-utils.cpp

--- src/utils/yeroth-erp-utils.cpp
+++ src/utils/yeroth-erp-utils.cpp
@@ -994,7 +994,7 @@ QString YerothUtils::GET_REFERENCE_RECU_SUFFIX(QString prefix,
 
 	QString referenceRecuSuffix(QString("%1-%2-%3")
 										.arg(QDate::currentDate().toString("yyyyMMdd"),
-										     QTime::currentTime().toString("hhmmss"),
+										     QTime::currentTime().toString("hhmm"),
 											 fixedNumber));
 	resultString.append(referenceRecuSuffix);
 
