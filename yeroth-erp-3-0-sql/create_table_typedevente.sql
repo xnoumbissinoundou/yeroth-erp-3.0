630f81b8427b687bcf5948bc75888397f5131ddd
Xavier NOUMBISSI NOUNDOU, Dipl.-Inf., Ph.D. (ABD) <xnoumbissi@gmail.com>
2020-05-02 21:45:34 +0100

    On branch master 	modified:   yeroth-erp-3-0-sql/create_table_typedevente.sql 	new file:   yeroth-erp-3-0-upgrade-sql/142d12e33c506f45bca853b78158c86242864f7b.sql

--- yeroth-erp-3-0-sql/create_table_typedevente.sql
+++ yeroth-erp-3-0-sql/create_table_typedevente.sql
@@ -1,4 +1,3 @@
 create table type_de_vente (id int primary key, type_de_vente int(12));
 insert into type_de_vente (id, type_de_vente) values (0, 350);
 insert into type_de_vente (id, type_de_vente) values (1, 360);
-insert into type_de_vente (id, type_de_vente) values (2, 370);
---
+++ yeroth-erp-3-0-upgrade-sql/142d12e33c506f45bca853b78158c86242864f7b.sql
@@ -0,0 +1,1 @@
+delete from type_de_vente where id='2';
