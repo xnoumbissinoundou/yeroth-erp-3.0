716eac5fdbbf83e37f8c35b965ba840521b6a845
Xavier NOUMBISSI NOUNDOU, Dipl.-Inf., Ph.D. (ABD) <xnoumbissi@gmail.com>
2020-05-03 19:36:20 +0100

    On branch master 	modified:   yeroth-erp-3-0-upgrade-sql/6789127ef66e98886889cc67bb821cc0d2cd75fa.sql

--- yeroth-erp-3-0-upgrade-sql/6789127ef66e98886889cc67bb821cc0d2cd75fa.sql
+++ yeroth-erp-3-0-upgrade-sql/6789127ef66e98886889cc67bb821cc0d2cd75fa.sql
@@ -7,13 +7,11 @@ DELETE FROM configurations WHERE nom_configuration='CONFIG_LATEX_SYSTEM_ROOT_FOL
 DELETE FROM configurations WHERE nom_configuration='CONFIG_THERMAL_PRINTER_DEVICE_FILE_FULL_PATH';
 DELETE FROM configurations WHERE nom_configuration='CONFIG_MAX_STRING_DISPLAY_LENGTH';
 DELETE FROM configurations WHERE nom_configuration='CONFIG_ANNEE_DEPART_RAPPORTS_CHIFFRE_AFFAIRE';
-
 update configurations SET id='0' where nom_configuration='ALERT_PERIOD_TIME_INTERVAL';
 update configurations SET id='1' where nom_configuration='ALERT_QUANTITY_TIME_INTERVAL';
 update configurations SET id='2' where nom_configuration='CONFIG_TVA_VALUE';
 update configurations SET id='3' where nom_configuration='CONFIG_SALES_STRATEGY';
 update configurations SET id='4' where nom_configuration='CONFIG_CURRENCY';
-
 DELETE FROM init_configurations WHERE nom_configuration='PDF_READER';
 DELETE FROM init_configurations WHERE nom_configuration='RECEIPT_FORMAT';
 DELETE FROM init_configurations WHERE nom_configuration='CONFIG_TEMPORARY_FILES_DIR';
@@ -23,13 +21,8 @@ DELETE FROM init_configurations WHERE nom_configuration='CONFIG_LATEX_SYSTEM_ROO
 DELETE FROM init_configurations WHERE nom_configuration='CONFIG_THERMAL_PRINTER_DEVICE_FILE_FULL_PATH';
 DELETE FROM init_configurations WHERE nom_configuration='CONFIG_MAX_STRING_DISPLAY_LENGTH';
 DELETE FROM init_configurations WHERE nom_configuration='CONFIG_ANNEE_DEPART_RAPPORTS_CHIFFRE_AFFAIRE';
-
 update init_configurations SET id='0' where nom_configuration='ALERT_PERIOD_TIME_INTERVAL';
 update init_configurations SET id='1' where nom_configuration='ALERT_QUANTITY_TIME_INTERVAL';
 update init_configurations SET id='2' where nom_configuration='CONFIG_TVA_VALUE';
 update init_configurations SET id='3' where nom_configuration='CONFIG_SALES_STRATEGY';
 update init_configurations SET id='4' where nom_configuration='CONFIG_CURRENCY';
-
-
-
-
