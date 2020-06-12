7e1d213a74b371979826ab71ea47666f39db3c4b
XAVIER NOUMBISSI NOUNDOU, DIPL.-INF., PH.D. (ABD) <jeovahnissi1982@gmail.com>
2020-06-12 02:32:12 -0400

    On branch master 	modified:   yeroth-erp-3-0-development-scripts/run-test-yeroth-erp-3-0-unit-testing.sh

--- yeroth-erp-3-0-development-scripts/run-test-yeroth-erp-3-0-unit-testing.sh
+++ yeroth-erp-3-0-development-scripts/run-test-yeroth-erp-3-0-unit-testing.sh
@@ -1,6 +1,56 @@
 #/bin/bash
 #@author: Xavier NOUMBISSI NOUNDOU, Dipl.-Inf., Ph.D. (ABD)
 
-${YEROTH_ERP_3_0_HOME_FOLDER}/bin/yeroth-erp-3.0-standalone > test-results-yeroth-erp-3-0.txt 2>&1
+USAGE="
+       ------------------------------------------------------------
+       'YEROTH-ERP-3.0' Unit Testing Running System
+        @auteur: Xavier NOUMBISSI NOUNDOU, DIPL.-INF., PH.D. (ABD)
+       ------------------------------------------------------------
+       Usage: $(basename $0)
+	[-h] : help
+	[-s] : simulate 'yeroth-erp-3.0' unit testing run
+	[-c] : run 'yeroth-erp-3.0' unit test and also generates
+	        coverage information with 'gcov'
+       ------------------------------------------------------------"
 
-run-test-yeroth-erp-3-0-coverage-gen-html.sh
+simulationFlag=
+coverageGenFlag=
+
+while getopts 'sch' OPTION
+do
+  case $OPTION in
+
+		h)	echo "$USAGE"
+				exit 1;
+		;;
+
+		s)	simulationFlag=1
+        echo "simulation activée."
+		;;
+
+    c)	coverageGenFlag=1
+		;;
+
+    ?)	printf "$USAGE\n" >&2
+        exit 2
+		;;
+
+  esac
+done
+shift $(($OPTIND - 1))
+
+
+if [ $simulationFlag ]; then
+		echo "${YEROTH_ERP_3_0_HOME_FOLDER}/bin/yeroth-erp-3.0-standalone > test-results-yeroth-erp-3-0.txt 2>&1"
+else
+		${YEROTH_ERP_3_0_HOME_FOLDER}/bin/yeroth-erp-3.0-standalone > test-results-yeroth-erp-3-0.txt 2>&1
+fi
+
+
+if [ $coverageGenFlag ]; then
+		if [ $simulationFlag ]; then
+				echo "run-test-yeroth-erp-3-0-coverage-gen-html.sh"
+		else
+				run-test-yeroth-erp-3-0-coverage-gen-html.sh
+		fi
+fi
