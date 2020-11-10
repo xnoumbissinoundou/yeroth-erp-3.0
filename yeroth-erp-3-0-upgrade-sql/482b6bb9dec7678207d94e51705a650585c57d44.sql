a69f5ffd2065c270a6a93fa4f873806f12faa790
DIPL.-INF. XAVIER NOUMBISSI NOUNDOU <jeovahnissi1982@gmail.com>
2020-11-10 00:02:48 -0500

    On branch master 	modified:   yeroth-erp-3-0-sql/create_table_achats.sql 	modified:   yeroth-erp-3-0-sql/create_table_stocks.sql 	modified:   yeroth-erp-3-0-upgrade-sql/482b6bb9dec7678207d94e51705a650585c57d44.sql

--- yeroth-erp-3-0-upgrade-sql/482b6bb9dec7678207d94e51705a650585c57d44.sql
+++ yeroth-erp-3-0-upgrade-sql/482b6bb9dec7678207d94e51705a650585c57d44.sql
@@ -1,2 +1,4 @@
+alter table stocks ADD column `montant_tva_en_gros` double AFTER montant_tva;
 alter table stocks ADD column `prix_vente_en_gros` double AFTER prix_vente;
+alter table achats ADD column `montant_tva_en_gros` double AFTER montant_tva;
 alter table achats ADD column `prix_vente_en_gros` double AFTER prix_vente;
