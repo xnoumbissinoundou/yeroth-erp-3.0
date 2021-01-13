cb0d99bfb001c28477f0ecdca581fa7706c43c28
DR. XAVIER NOUMBISSI NOUNDOU <yerotherp30@gmail.com>
2021-01-13 12:36:15 -0500

    updated

--- src/windows/crm/yeroth-erp-programmes-de-fidelite-clients-window.cpp
+++ src/windows/crm/yeroth-erp-programmes-de-fidelite-clients-window.cpp
@@ -399,11 +399,10 @@ void YerothERPProgrammesDeFideliteClientsWindow::reinitialiser_champs_db_visible
 void YerothERPProgrammesDeFideliteClientsWindow::contextMenuEvent(QContextMenuEvent * event)
 {
 	QMenu menu(this);
-	menu.setPalette(toolBar_programmes_de_fidelite_clientsWindow->palette());
 
+	menu.setPalette(toolBar_programmes_de_fidelite_clientsWindow->palette());
 	menu.addAction(actionAfficher_ce_programmeDeFideliteClients_au_detail);
 	menu.addAction(actionSupprimerUnProgrammeDeFideliteClients);
-
 	menu.exec(event->globalPos());
 }
 
