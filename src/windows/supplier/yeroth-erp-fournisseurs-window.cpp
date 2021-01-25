680e82308d30febd653b450d7dce34034639df92
DR. XAVIER NOUMBISSI NOUNDOU <yerotherp30@gmail.com>
2021-01-24 20:22:12 -0500

    updated.

--- src/windows/supplier/yeroth-erp-fournisseurs-window.cpp
+++ src/windows/supplier/yeroth-erp-fournisseurs-window.cpp
@@ -52,6 +52,7 @@ YerothERPFournisseursWindow::YerothERPFournisseursWindow()
 
     _list_yeroth_pushbutton_to_enable_on_positive_tableview_ROW_COUNT
 		<< pushButton_afficher
+		<< pushButton_modifier
 		<< pushButton_supprimer;
 
 
