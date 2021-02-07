0f2a10c917b15b6a48f90389f58384d3e0c4cff4
DR. XAVIER NOUMBISSI NOUNDOU <yerotherp30@gmail.com>
2021-02-07 00:13:41 -0500

    updated.

--- yeroth-erp-3-0-upgrade-sql/1b6a58df949479d1af21758714965048a4fc2aac.sql
+++ yeroth-erp-3-0-upgrade-sql/1b6a58df949479d1af21758714965048a4fc2aac.sql
@@ -1,1 +1,2 @@
+alter table comptes_bancaires modify column `reference_du_compte_bancaire` varchar(256) UNIQUE NOT NULL;
 alter table comptes_bancaires modify column `intitule_du_compte_bancaire` varchar(256) UNIQUE NOT NULL;
