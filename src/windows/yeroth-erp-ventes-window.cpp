03219d65caf7198c1f36b1993861f4fe1f088fbf
DR. XAVIER NOUMBISSI NOUNDOU <yerotherp30@gmail.com>
2021-02-07 18:41:40 -0500

    updated !

--- src/windows/yeroth-erp-ventes-window.cpp
+++ src/windows/yeroth-erp-ventes-window.cpp
@@ -336,7 +336,7 @@ bool YerothVentesWindow::annuler_cette_vente()
 		return false;
 	}
 
-	int typeDeVente = YerothUtils::VENTE_INDEFINI;
+	int typeDeVente = YerothUtils::VENTE_COMPTANT;
 
 	QString curNomDuClient;
 
