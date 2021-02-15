5980068f55eb66f53fccdf67e8561ba185247fa4
DR. XAVIER NOUMBISSI NOUNDOU <yerotherp30@gmail.com>
2021-02-15 01:28:41 -0500

    On branch master 	modified:   yeroth-create-dpkg_yeroth-erp-3-0-standalone.sh

--- yeroth-erp-3-0-development-scripts/yeroth-create-dpkg_yeroth-erp-3-0-standalone.sh
+++ yeroth-erp-3-0-development-scripts/yeroth-create-dpkg_yeroth-erp-3-0-standalone.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sudo rm -rf yeroth-erp-3.0-standalone
+sudo rm -rf "${YEROTH_ERP_3_0_HOME_FOLDER}/yeroth-erp-3.0-standalone"
 
-yeroth-create-dpkg_yeroth-erp-3-0.sh -v standalone
+"${YEROTH_ERP_3_0_HOME_FOLDER}"/yeroth-erp-3-0-development-scripts/yeroth-create-dpkg_yeroth-erp-3-0.sh -v standalone
