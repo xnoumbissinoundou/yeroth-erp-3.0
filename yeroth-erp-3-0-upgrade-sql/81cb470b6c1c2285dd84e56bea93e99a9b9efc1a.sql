2307e5b083280c7e71add3bb7481d50c9eb32e18
DR. XAVIER NOUMBISSI NOUNDOU <yerotherp30@gmail.com>
2021-02-26 08:39:42 -0500

    updated.

--- yeroth-erp-3-0-upgrade-sql/81cb470b6c1c2285dd84e56bea93e99a9b9efc1a.sql
+++ yeroth-erp-3-0-upgrade-sql/81cb470b6c1c2285dd84e56bea93e99a9b9efc1a.sql
@@ -1,3 +1,3 @@
-UPDATE init_configurations SET valeur_configuration='/opt/yeroth-erp-3.0-sql-backup-restore' WHERE nom_configuration='DIRECTORY_FULL_PATH_FOR_BACKUP_DATABASE_YEROTH_ERP_3';
+UPDATE init_configurations SET valeur_configuration='/opt/yeroth-erp-3-0-sql-backup-restore' WHERE nom_configuration='DIRECTORY_FULL_PATH_FOR_BACKUP_DATABASE_YEROTH_ERP_3';
 
-UPDATE configurations SET valeur_configuration='/opt/yeroth-erp-3.0-sql-backup-restore' WHERE nom_configuration='DIRECTORY_FULL_PATH_FOR_BACKUP_DATABASE_YEROTH_ERP_3';
+UPDATE configurations SET valeur_configuration='/opt/yeroth-erp-3-0-sql-backup-restore' WHERE nom_configuration='DIRECTORY_FULL_PATH_FOR_BACKUP_DATABASE_YEROTH_ERP_3';
