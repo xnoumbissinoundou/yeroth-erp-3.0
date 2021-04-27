d14fd85770861d54442f8aba3a94bc637f03b40b
PR. XAVIER NOUMBISSI NOUNDOU <yeroth.rd.30@gmail.com>
2021-04-27 15:20:36 -0400

    UPDATED.

---
+++ yeroth-erp-3-0-upgrade-sql/118491ab7d4ecccd40c9c3010565810cdd1c86bb.sql
@@ -0,0 +1,4 @@
+alter table programmes_de_fidelite_clients DROP column pourcentage_du_rabais_refereur;
+alter table programmes_de_fidelite_clients DROP column montant_du_rabais;
+alter table programmes_de_fidelite_clients DROP column rabais_en_cascade_tous_refereurs_par_le_haut;
+alter table programmes_de_fidelite_clients DROP column valeur_de_la_condition_beneficiaire;
