d30e9a14f1ea14c95b4505c284c5aa51db1e822d
PR. XAVIER NOUMBISSI NOUNDOU <yeroth.rd.30@gmail.com>
2021-05-12 05:23:35 -0400

    UPDATED.

--- yeroth-erp-3-0-upgrade-sql/cdff2188a7a4fc799d7c22ddca25e136d0333a58.sql
+++ yeroth-erp-3-0-upgrade-sql/cdff2188a7a4fc799d7c22ddca25e136d0333a58.sql
@@ -1,6 +1,6 @@
 alter table imprimantereseau_recus_petits change column `nom_utilisateur_denvoi` nom_utilisateur varchar(256);
 alter table imprimantereseau_recus_petits change column `message_recu_petit` message_PDV_recu_petit varchar(256);
-alter table imprimantereseau_recus_petits change column `message_PDV_recu_petit` message_PDV_recu_petit varchar(256) NOT NULL;
+alter table imprimantereseau_recus_petits change column `message_PDV_recu_petit` message_PDV_recu_petit varchar(12000) NOT NULL;
 alter table imprimantereseau_recus_petits change column `nom_utilisateur` nom_utilisateur varchar(256) NOT NULL;
 alter table imprimantereseau_recus_petits ADD column `nom_complet` varchar(256) NOT NULL AFTER nom_utilisateur;
 alter table imprimantereseau_recus_petits ADD column `adresse_ip_reseau_imprimante_thermique` varchar(15) NOT NULL AFTER message_PDV_recu_petit;
