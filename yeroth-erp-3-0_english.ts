3e25fad4d72e89cae2845c2f63c315f426af5c1a
PR. XAVIER NOUMBISSI NOUNDOU <yeroth.rd.30@gmail.com>
2021-05-17 23:43:19 -0400

    On branch master 	modified:   yeroth-erp-3-0_english.ts

--- yeroth-erp-3-0_english.ts
+++ yeroth-erp-3-0_english.ts
@@ -4430,7 +4430,7 @@ Contactez l&apos;administrateur de Yeroth-erp-3.0 !</oldsource>
     </message>
     <message>
         <location filename="src/admin/yeroth-erp-admin-windows-commons.cpp" line="65"/>
-        <location filename="src/windows/yeroth-erp-window-commons.cpp" line="1175"/>
+        <location filename="src/windows/yeroth-erp-window-commons.cpp" line="1177"/>
         <source>qui suis je ?</source>
         <translation>who am I ?</translation>
     </message>
@@ -4616,7 +4616,7 @@ Contactez l&apos;administrateur de Yeroth-erp-3.0 !</oldsource>
         <translation>missing MANDATORY column</translation>
     </message>
     <message>
-        <location filename="src/windows/yeroth-erp-window-commons.cpp" line="1162"/>
+        <location filename="src/windows/yeroth-erp-window-commons.cpp" line="1164"/>
         <source>%1 - sélectionner les colones visibles (&apos;%2&apos; colones)</source>
         <translation>%1 - select visible columns (&apos;%2&apos; columns)</translation>
     </message>
@@ -5164,14 +5164,14 @@ Please, change the maximum member of this CLIENT LOYALTY group to &apos;%3&apos;
     <message>
         <location filename="src/dialogs/yeroth-erp-IMPRESSION_DE_DOCUMENT-dialog.cpp" line="238"/>
         <location filename="src/dialogs/yeroth-erp-IMPRESSION_DE_DOCUMENT-dialog.cpp" line="337"/>
-        <location filename="src/windows/yeroth-erp-window-commons.cpp" line="960"/>
+        <location filename="src/windows/yeroth-erp-window-commons.cpp" line="962"/>
         <source>IMPRESSION, NUMÉRO DE PAGES</source>
         <translation>PRINTING, PAGE NUMBERING</translation>
     </message>
     <message>
         <location filename="src/dialogs/yeroth-erp-IMPRESSION_DE_DOCUMENT-dialog.cpp" line="239"/>
         <location filename="src/dialogs/yeroth-erp-IMPRESSION_DE_DOCUMENT-dialog.cpp" line="338"/>
-        <location filename="src/windows/yeroth-erp-window-commons.cpp" line="961"/>
+        <location filename="src/windows/yeroth-erp-window-commons.cpp" line="963"/>
         <source>La numéro de la page de départ doit être inférieur au numéro de la page terminale</source>
         <translation>starting page number  must be inferior to ending PAGE NUMBER</translation>
     </message>
